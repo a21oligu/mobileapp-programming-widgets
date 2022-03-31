a94f0d6f8281f5bd247d4f84ab72262e9c306889
Oliver Gustafsson <oliver.gustafson17@gmail.com>
2022-03-31 10:27:09 +0200

    created instances of components

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.widgets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private Button buttonDisplayText;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.edit_text);
+        buttonDisplayText = findViewById(R.id.button_display_text);
+        textView = findViewById(R.id.final_text_view);
+
+        buttonDisplayText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText(editText.getText());
+            }
+        });
+
     }
+
+
+
 }
